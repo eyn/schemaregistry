3666ca78fdee1b6ad798d40c7e622d6fa9f1d6d9
James Moore <james.moore@cantab.net>
2016-02-10 15:44:23 +0000

    Initial test skeleton

---
+++ schemaregistry/tests/test_memorystorage.py
@@ -0,0 +1,15 @@
+"""
+    tests.memorystorage
+    ~~~~~~~~~~~~~~~~~~~
+
+    Tests the Memory Storage container.
+
+    :copyright: (c) 2016 by James Moore.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import pytest
+from schemaregistry.storage.memory import Memory
+
+def test_create_schema():
+    pass
